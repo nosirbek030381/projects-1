ddb4a487db5a828937afb32f5258c219b7fa6b9a
nosirbek030381 <ismoilovn899@gmail.com>
2024-08-08 23:44:39 +0500

    uichart

--- src/components/ProductChart.tsx
+++ src/components/ProductChart.tsx
@@ -1,21 +1,21 @@
-import { fetchPosts } from '@/slice/post.slice';
-import { AppDispatch, RootState } from '@/store';
-import { useEffect } from 'react';
-import { Doughnut } from 'react-chartjs-2';
-import { useDispatch, useSelector } from 'react-redux';
+// import { fetchPosts } from '@/slice/post.slice';
+// import { AppDispatch, RootState } from '@/store';
+// import { useEffect } from 'react';
+// import { Doughnut } from 'react-chartjs-2';
+// import { useDispatch, useSelector } from 'react-redux';
 
-const ProductChart = () => {
-	const dispatch = useDispatch<AppDispatch>();
-	const { posts, status } = useSelector((state: RootState) => state.posts);
+// const ProductChart = () => {
+// 	const dispatch = useDispatch<AppDispatch>();
+// 	const { products, status } = useSelector((state: RootState) => state.products);
 
-	useEffect(() => {
-		dispatch(fetchPosts());
-	}, [dispatch]);
+// 	useEffect(() => {
+// 		dispatch(fetchPosts());
+// 	}, [dispatch]);
 
-	if (status === 'loading') return <p>Loading...</p>;
-	if (status === 'failed') return <p>Failed to load data</p>;
+// 	if (status === 'loading') return <p>Loading...</p>;
+// 	if (status === 'failed') return <p>Failed to load data</p>;
 
-	return <Doughnut data={chartData} />;
-};
+// 	return <Doughnut data={products} />;
+// };
 
-export default ProductChart;
+// export default ProductChart;
